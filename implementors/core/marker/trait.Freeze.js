95d30fd76ed493c3086feb1154a9c2209fb8c911
Travis CI User <travis@example.org>
2019-09-21 19:48:05 +0000

    Update documentation

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["clip"] = [{text:"impl Freeze for <a class=\"struct\" href=\"clip/struct.ClipImage.html\" title=\"struct clip::ClipImage\">ClipImage</a>",synthetic:true,types:["clip::clip_image::ClipImage"]},{text:"impl Freeze for <a class=\"struct\" href=\"clip/struct.Clip.html\" title=\"struct clip::Clip\">Clip</a>",synthetic:true,types:["clip::Clip"]},{text:"impl Freeze for <a class=\"enum\" href=\"clip/enum.ClipFormat.html\" title=\"enum clip::ClipFormat\">ClipFormat</a>",synthetic:true,types:["clip::ClipFormat"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["clip"] = {"name":"","files":["clip_image.rs","lib.rs"]};
+createSourceSidebar();
