f2848b320b81e237a8a1d8c82203d588cf34adcc
SpiralP <spiral.palmer@gmail.com>
2020-03-02 05:01:26 -0800

    upgrade image, fix code to match version

--- src/clip_image.rs
+++ src/clip_image.rs
@@ -1,19 +1,31 @@
 use clip_sys::{clip_delete_image, clip_get_image_data, clip_get_image_spec, clip_image_spec};
 use failure::{err_msg, Error};
-use image::{bmp::BMPEncoder, jpeg::JPEGEncoder, png::PNGEncoder, ColorType};
-use std::{io, io::Write, os::raw::c_void};
+use image::{bmp::BMPEncoder, jpeg::JPEGEncoder, png::PNGEncoder, ColorType, ImageError};
+use std::{io::Write, os::raw::c_void};
 
 pub struct ClipImage {
   ptr: *mut c_void,
 }
 
 pub trait Encoder {
-  fn encode(self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()>;
+  fn encode(
+    self,
+    data: &[u8],
+    width: u32,
+    height: u32,
+    color_type: ColorType,
+  ) -> Result<(), ImageError>;
 }
 
 impl<W: Write> Encoder for PNGEncoder<W> {
-  fn encode(self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()> {
-    PNGEncoder::encode(self, data, width, height, color)
+  fn encode(
+    self,
+    data: &[u8],
+    width: u32,
+    height: u32,
+    color_type: ColorType,
+  ) -> Result<(), ImageError> {
+    PNGEncoder::encode(self, data, width, height, color_type)
   }
 }
 
@@ -21,8 +33,14 @@ impl<'a, W: 'a> Encoder for JPEGEncoder<'a, W>
 where
   W: Write,
 {
-  fn encode(mut self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()> {
-    JPEGEncoder::encode(&mut self, data, width, height, color)
+  fn encode(
+    mut self,
+    data: &[u8],
+    width: u32,
+    height: u32,
+    color_type: ColorType,
+  ) -> Result<(), ImageError> {
+    JPEGEncoder::encode(&mut self, data, width, height, color_type)
   }
 }
 
@@ -30,8 +48,14 @@ impl<'a, W: 'a> Encoder for BMPEncoder<'a, W>
 where
   W: Write,
 {
-  fn encode(mut self, data: &[u8], width: u32, height: u32, color: ColorType) -> io::Result<()> {
-    BMPEncoder::encode(&mut self, data, width, height, color)
+  fn encode(
+    mut self,
+    data: &[u8],
+    width: u32,
+    height: u32,
+    color_type: ColorType,
+  ) -> Result<(), ImageError> {
+    BMPEncoder::encode(&mut self, data, width, height, color_type)
   }
 }
 
@@ -66,33 +90,39 @@ impl<'a> ClipImage {
     let clip_spec = self.get_spec();
     let clip_data = self.get_data();
 
-    let bits_per_pixel = clip_spec.bits_per_pixel;
-    let bytes_in_pixel = (bits_per_pixel / 8) as usize;
     let width = clip_spec.width as u32;
     let height = clip_spec.height as u32;
 
     let color_type = color_type_from_spec(&clip_spec)
       .ok_or_else(|| err_msg("color_type_from_spec unhandled spec type"))?;
 
+    let bits_per_pixel = clip_spec.bits_per_pixel;
+    let bytes_in_pixel = (bits_per_pixel / 8) as usize;
+
+    // our png/jpeg encoders only support RGB, so we convert BGR
     match color_type {
-      ColorType::BGRA(_) | ColorType::BGR(_) => {
-        let mut converted_data = clip_data.to_vec();
-        for i in (0..converted_data.len()).step_by(bytes_in_pixel) {
-          converted_data.swap(i, i + 2); // swap blue and red
+      ColorType::Bgra8 => {
+        let mut clip_data = clip_data.to_vec();
+
+        for i in (0..clip_data.len()).step_by(bytes_in_pixel) {
+          clip_data.swap(i, i + 2); // swap blue and red
         }
 
-        let color_type = if bytes_in_pixel == 4 {
-          // we have alpha
-          ColorType::RGBA(8)
-        } else {
-          ColorType::RGB(8)
-        };
+        encoder.encode(&clip_data, width, height, ColorType::Rgba8)?;
+      }
 
-        encoder.encode(&converted_data, width, height, color_type)?;
+      ColorType::Bgr8 => {
+        let mut clip_data = clip_data.to_vec();
+
+        for i in (0..clip_data.len()).step_by(bytes_in_pixel) {
+          clip_data.swap(i, i + 2); // swap blue and red
+        }
+
+        encoder.encode(&clip_data, width, height, ColorType::Rgb8)?;
       }
 
       _ => {
-        encoder.encode(clip_data, width, height, color_type)?;
+        encoder.encode(&clip_data, width, height, color_type)?;
       }
     }
 
@@ -116,11 +146,30 @@ fn color_type_from_spec(spec: &clip_image_spec) -> Option<ColorType> {
       spec.blue_shift,
       spec.alpha_shift,
     ) {
-      (0, 8, 16, 24) => ColorType::RGBA((spec.bits_per_pixel / 4) as u8),
-      (0, 8, 16, 0) => ColorType::RGB((spec.bits_per_pixel / 3) as u8),
+      // R G B A
+      (0, 8, 16, 24) => match spec.bits_per_pixel / 4 {
+        8 => ColorType::Rgba8,
+        16 => ColorType::Rgba16,
+        _ => unreachable!(),
+      },
+      // R G B
+      (0, 8, 16, 0) => match spec.bits_per_pixel / 3 {
+        8 => ColorType::Rgb8,
+        16 => ColorType::Rgb16,
+        _ => unreachable!(),
+      },
 
-      (16, 8, 0, 24) => ColorType::BGRA((spec.bits_per_pixel / 4) as u8),
-      (16, 8, 0, 0) => ColorType::BGR((spec.bits_per_pixel / 3) as u8),
+      // B G R A
+      (16, 8, 0, 24) => match spec.bits_per_pixel / 4 {
+        8 => ColorType::Bgra8,
+        _ => unreachable!(),
+      },
+
+      // B G R
+      (16, 8, 0, 0) => match spec.bits_per_pixel / 3 {
+        8 => ColorType::Bgr8,
+        _ => unreachable!(),
+      },
 
       _ => return None,
     },
