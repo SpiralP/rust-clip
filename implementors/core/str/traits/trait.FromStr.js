62737575f05da8fdec89a133bc213f280be19da6
SpiralP <SpiralP@users.noreply.github.com>
2020-11-28 03:43:48 +0000

    deploy: d40a7f31e3bddff14ace88a29b68c97063e11095

---
+++ implementors/core/str/traits/trait.FromStr.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["num_rational"] = [{"text":"impl&lt;T:&nbsp;FromStr + Clone + Integer&gt; FromStr for Ratio&lt;T&gt;","synthetic":false,"types":[]}];
+implementors["proc_macro2"] = [{"text":"impl FromStr for TokenStream","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
